4f37bfab4684eb44b380f31dcf6987c9d9afae3e
Vadim <mysh_320@mail.ru>
2022-12-26 16:04:47 +0300

    задача 27

---
+++ DZ4Z27/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Task27();
+
+void Task27()
+{
+
+    int number = ReadInt(" ");
+    if (number >=9)
+    {
+    int sum = NumberLen(number);
+    SumNumbers(number, sum);
+    }
+    else 
+    {
+        Console.WriteLine($"Число некорректное!");
+    }
+}
+
+// Функция ввода
+int ReadInt(string argumentName)
+{
+	Console.Write($"Введите число {argumentName}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a = a / 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int sum)
+{
+    int sumA = 0;
+    for (int i = 1; i <= sum; i++)
+    {
+        sumA = sumA + n % 10;
+        n = n / 10;
+    }
+    Console.WriteLine(sumA);
+}
+
